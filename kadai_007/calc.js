3e1e023f41e222684eb88dd3b4b26c65eaf98ee2
yuyu-78 <160311234+yuyu-78@users.noreply.github.com>
2024-04-14 09:31:45 +0900

    Add files via upload
    課題007提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+let num = 15;
+
+console.log (num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+else if (num === num % 5 === 0) {
+    console.log('5の倍数です');
+  }
+
+else {
+    console.log(num);
+}
