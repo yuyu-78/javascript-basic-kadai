75627387b8db2e38786440ad9fc1d15451f62b99
yuyu-78 <160311234+yuyu-78@users.noreply.github.com>
2024-04-19 13:45:06 +0900

    Add files via upload
    課題mdn提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = today.getFullYear();
+
+const month = today.getMonth();
+
+const day = today.getDate();
+
+console.log (year + '年' + month + '月' + day + '日');
